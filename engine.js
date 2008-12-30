07769038391c19cea97334aef7fa2bdc0cfdf2c5
sokolov.denis <sokolov.denis@87d332b4-d419-11dd-918e-25ffe7a15323>
2008-12-30 12:38:57 +0000

    Clean up.
    git-svn-id: https://magic-drafting-application.googlecode.com/svn/trunk@20 87d332b4-d419-11dd-918e-25ffe7a15323

--- engine.js
+++ engine.js
@@ -10,24 +10,21 @@
 //
 //      You should have received a copy of the GNU General Public License
 //      along with this program; if not, see: <http://www.gnu.org/licenses/>
-window.constants = {
+constants = {
 	tick: 250,
 };
 
-$(document).ready(function(){
-	if (typeof window.debug == 'undefined')
-		window.debug = false;
-	if (window.debug)
-	{
-		$('body').addClass('debug').append('<div class="debugMessage">Debug mode is on.'
-			+' <a>Turn off.</a></div>');
-		$('.debugMessage a').click(function(){
-			$(this).parent().hide('slow');
-			window.debug = false;
-		});
-	}
 
-	/* Movement events */
+$(document).ready(function(){
+	debug.setup();
+	controls.setup();
+	settings.load();
+	navigation.first();
+	$('.loading').hide();
+});
+
+controls = {
+	keyboard: function() {
 		$('html').keypress(function (e) {
 			if (e.which == 32) // space
 			{
@@ -37,238 +34,232 @@ $(document).ready(function(){
 					case 'a': $(e.target).click(); break;
 					default:
 						if ($('#end.current, .current.freeze').length < 1)
-							next();
+							navigation.next();
 					break;
 				}
 			}
 		});
+	},
 
-		bindButtons();
+	mouse: function() {
+		$('button').unbind('mouseup').mouseup(function(){
+			if ($('.current.freeze').length < 1)
+				navigation.next();
+		});
+	},
 
+	internalLinks: function() {
 		$('a[href^="#"]').click(function(e){
 			e.preventDefault();
 			show($($(this).attr('href')));
 		});
+	},
 
-	$('.loading').hide();
-	window.settings.load();
-	next();
-});
-
-function prev()
-{
-	var me, curr;
-
-	curr = $('#steps li.current');
-	if (curr.length)
-	{
-		curr.removeClass('current');
-		me = curr.prev();
-	}
-	else
-		me = $('#steps li').eq(0);
-	show(me);
+	setup: function() {
+		this.keyboard();
+		this.mouse();
+		this.internalLinks();
+	},
 }
 
-function curr()
-{
-	var me = $('#steps li.current');
-	if (!me.length)
-		me = $('#steps li').eq(0);
-	show(me);
-}
-
-function next()
-{
-	var me, curr;
-
-	curr = $('#steps li.current');
-	if (curr.length)
-	{
-		curr.removeClass('current');
-		me = curr.next();
-	}
-	else
-		me = $('#steps li').eq(0);
-	show(me);
-}
-
-function show(me)
-{
-	$('.current').removeClass('current');
-	stopTimers();
-
-	if (me.attr('id') == 'begin')
-		prepareDraft();
-
-	me.addClass('current');
-	if (me.find('button').length)
-		me.find('button').focus();
-
-	if (me.find('var').length)
-	{
-		me
-			// Disallow continuing before the timer runs out
-			.addClass('freeze')
-			.find('button').css('opacity', 0).end()
-			.find('.timerControls .skip').show() // Show the hidden skip
-			;
-
-		var delay = 0;
-		if (me.find('.pre').length)
+debug = {
+	state: false,
+	on: function() { this.state = true; },
+	off: function() { this.state = false; },
+	setup: function(){
+		if (this.state)
 		{
-			delay = me.find('.pre').attr('title')*1000;
-			me.find('.pre').animate({opacity: 0}, delay + 1000);
-		}
-
-		// Set timer to make a delay
-		setTimer(function(me){
-			var timeDisplay = me.find('var');
-			timerHelper.element = timeDisplay;
-			timeDisplay.data('finished', me.find('samp').text());
-			var timeout = timeDisplay.attr('title') * 1000;
-
-			me.find('p').each(function(){
-				var me = $(this)
-				if (me.hasClass('pulse'))
-				{
-					setTimer(function(el){
-						el.css('visibility', 'visible').animate({opacity: 0}, 3000);
-						},
-						timeout - me.attr('title')*1000, me);
-					}
+			$('body').addClass('debug').append('<div class="debugMessage">Debug mode is on.'
+				+' <a>Turn off.</a></div>');
+			$('.debugMessage a').click(function(){
+				$(this).parent().hide('slow');
+				debug = false;
 			});
-
-			timerHelper(timeout);
-
-			me.find('var').add('.timerControls', me).add('.info', me).css('visibility', 'visible');
-
-		}, delay, me);
+		}
 	}
 }
 
-function timerHelper(remaining)
-{
-	if (remaining <= 0)
-	{
-		timerHelper.element.text(timerHelper.element.data('finished'));
-		timerHelper.element.parent()
-			.removeClass('freeze')
-			.find('button').show()
-				.animate({opacity: 1}, 1000).focus()
-				.end()
-			.find('.timerControls .skip').fadeOut();
+navigation = {
+	first: function() { this.show(); },
+	prev: function() { this.show($('#steps li.current').prev()); },
+	curr: function() { this.show($('#steps li.current')); },
+	next: function() { this.show($('#steps li.current').next()); },
 
+	show: function(me) {
+		timers.stop();
+		me = this.getMe(me);
 
-		return;
+		if (me.attr('id') == 'begin')
+			preparation.run();
+
+		$('.current').removeClass('current');
+		me.addClass('current')
+			.find('button').focus();
+
+		// If this is timered
+		if (me.find('var').length)
+		{
+			me
+				// Disallow continuing before the timer runs out
+				.addClass('freeze')
+				.find('button').css('opacity', 0).end()
+				.find('.timerControls .skip').show() // Show the hidden skip
+				;
+
+			var delay = 0;
+			if (me.find('.pre').length)
+			{
+				delay = me.find('.pre').attr('title')*1000;
+				me.find('.pre').animate({opacity: 0}, delay + 1000);
+			}
+
+			// Set timer to make a delay
+			timers.set(function(me){
+				var timeDisplay = me.find('var');
+				step.timerHelper.element = timeDisplay;
+				timeDisplay.data('finished', me.find('samp').text());
+				var timeout = timeDisplay.attr('title') * 1000;
+
+				me.find('p').each(function(){
+					var me = $(this)
+					if (me.hasClass('pulse'))
+					{
+						timers.set(function(el){
+							el.css('visibility', 'visible').animate({opacity: 0}, 3000);
+							},
+							timeout - me.attr('title')*1000, me);
+						}
+				});
+
+				step.timerHelper(timeout);
+
+				me.find('var').add('.timerControls', me).add('.info', me).css('visibility', 'visible');
+
+			}, delay, me);
+		}
+	},
+
+	// Internal
+	getMe: function(me) {
+		if (typeof me == 'undefined' || typeof me.length == 'undefined' || me.length < 1)
+			me = $('#steps li').eq(0);
+		return me;
 	}
-	timerHelper.element.text((remaining/1000).toFixed(1));
-	setTimer(timerHelper, window.constants.tick, (remaining-window.constants.tick));
-}
+};
+
+step = {
+	timerHelper: function(remaining) {
+		if (remaining <= 0)
+		{
+			step.timerHelper.element.text(step.timerHelper.element.data('finished'));
+			step.timerHelper.element.parent()
+				.removeClass('freeze')
+				.find('button').show()
+					.animate({opacity: 1}, 1000).focus()
+					.end()
+				.find('.timerControls .skip').fadeOut();
 
 
-function setTimer(f,d,a)
-{
-	//console.debug('Setting timer for ', f, ' in ', d, ' with args ', a);
-	var id = setTimeout(f,d,a);
-	if (typeof setTimer.place == 'undefined')
-		setTimer.place = [];
-	setTimer.place.push(id);
-}
+			return;
+		}
+		step.timerHelper.element.text((remaining/1000).toFixed(1));
+		timers.set(step.timerHelper, constants.tick, (remaining-constants.tick));
+	}
+};
 
-function stopTimers()
-{
-	var b;
-	if (typeof setTimer.place == 'undefined')
-		return;
-	while (b = setTimer.place.pop())
-		clearTimeout(b);
-}
-
+timers = {
+	data: [],
+	set: function(f,d,a) { timers.data.push(setTimeout(f,d,a)); },
+	stop: function() {
+		var b;
+		while (b = this.data.pop())
+			clearTimeout(b);
+	}
+};
 
 
-function templates()
-{
-	/* Template steps */
-	$('li.simpleBooster').each(function(){
-		var me = $(this);
-		var content = '';
-		var boosterTitle = me.attr('title');
-		var simpleBooster = [35, 35, 35, 25, 25, 25, 20, 20, 20, 10, 10, 5, 5, 5];
+preparation = {
+	run: function() {
+		settings.save();
+		this.templates();
+		this.timers();
+		// Setting the texts in the engine
+		if (settings.get('s-14cardPack'))
+			$('.v-cardsInBooster').text('14');
+		for (var i=1;i<4;++i)
+			$('.v-set'+i).text('‘'+settings.get('s-set'+i)+'’');
+	},
 
-		if ($('#s-14cardPack').attr('checked'))
-			simpleBooster.shift();
+	templates: function(){
+		/* Template steps */
+		$('li.simpleBooster').each(function(){
+			var me = $(this);
+			var content = '';
+			var boosterTitle = me.attr('title');
+			var simpleBooster = [35, 35, 35, 25, 25, 25, 20, 20, 20, 10, 10, 5, 5, 5];
 
-		//$([5]).each(function(no){
-		$(simpleBooster).each(function(no){
-			var warn = 10;
-			if (this < 15) warn = 5;
-			if (this < 10) warn = 0;
-			content += '<li class="timer">'
-				+'<p class="pre" title="1">Look!</p><var>'+this+'</var>';
-			if (warn > 0)
-				content += '<p class="pulse" title="'+warn+'">'+warn+' seconds!</p>';
-			content += '<samp>Pass '+(14-no)+' card'+(no<13?'s':'')+'.</samp><button>Done.</button>'
-			+'<p class="info">Card '+(no+1)+'<br>'+boosterTitle+'</p>'
-			+'</li>';
+			if ($('#s-14cardPack').attr('checked'))
+				simpleBooster.shift();
+
+			//$([5]).each(function(no){
+			$(simpleBooster).each(function(no){
+				var warn = 10;
+				if (this < 15) warn = 5;
+				if (this < 10) warn = 0;
+				content += '<li class="timer">'
+					+'<p class="pre" title="1">Look!</p><var>'+this+'</var>';
+				if (warn > 0)
+					content += '<p class="pulse" title="'+warn+'">'+warn+' seconds!</p>';
+				content += '<samp>Pass '+(14-no)+' card'+(no<13?'s':'')+'.</samp><button>Done.</button>'
+				+'<p class="info">Card '+(no+1)+'<br>'+boosterTitle+'</p>'
+				+'</li>';
+			});
+			me.after(content);
+			me.remove();
+			return;
 		});
-		me.after(content);
-		me.remove();
-		return;
-	});
-}
+	},
 
-function prepareDraft()
-{
-	window.settings.save();
-	templates();
-	timers();
-	// Setting the texts in the engine
-	if (window.settings.get('s-14cardPack'))
-		$('.v-cardsInBooster').text('14');
-	for (var i=1;i<4;++i)
-		$('.v-set'+i).text('‘'+window.settings.get('s-set'+i)+'’');
-}
-
-function timers()
-{
-	/* Timers */
-	$('var').each(function(){
-		var me = $(this);
-		me.attr('title',me.text() + '.0');
-		me.parents('li').addClass('timer');
-	});
-
-	$('li var').parent().append('<p class="timerControls">'
-		+'<a href="" class="restart">Restart this timer</a> '
-		+'<a href="" class="skip">Skip this timer</a>'
-		+'</p>');
-	$('.timerControls')
-		.find('.restart').click(function(e){
-			e.preventDefault();
-			stopTimers();
-			$(this).parents('li')
-				.fadeOut(function(){
-					var me = $(this);
-					me
-						.find('var').text(me.find('var').attr('title')).end()
-						.fadeIn();curr();
-				});
-		}).end()
-		.find('.skip').click(function(e){
-			e.preventDefault();
-			stopTimers();
-			$(this).parents('ul')
-				.fadeOut('fast', function(){
-					$(this).fadeIn();
-					next();
-				});
+	timers: function(){
+		/* Timers */
+		$('var').each(function(){
+			var me = $(this);
+			me.attr('title',me.text() + '.0');
+			me.parents('li').addClass('timer');
 		});
-	bindButtons();
-}
+
+		$('li var').parent().append('<p class="timerControls">'
+			+'<a href="" class="restart">Restart this timer</a> '
+			+'<a href="" class="skip">Skip this timer</a>'
+			+'</p>');
+		$('.timerControls')
+			.find('.restart').click(function(e){
+				e.preventDefault();
+				timers.stop();
+				$(this).parents('li')
+					.fadeOut(function(){
+						var me = $(this);
+						me
+							.find('var').text(me.find('var').attr('title')).end()
+							.fadeIn();
+						navigation.curr();
+					});
+			}).end()
+			.find('.skip').click(function(e){
+				e.preventDefault();
+				timers.stop();
+				$(this).parents('ul')
+					.fadeOut('fast', function(){
+						$(this).fadeIn();
+						navigation.next();
+					});
+			});
+		controls.mouse();
+	}
+};
 
 // Limit of 20 cookies
-window.settings = {
+settings = {
 	get: function(k){
 		var me = $('#'+k);
 		switch (me.attr('type'))
@@ -297,16 +288,16 @@ window.settings = {
 		$('#settings')
 			.find('input, select').each(function(){
 				var id = $(this).attr('id');
-				window.cookies.set(id, window.settings.get(id), 'max');
+				cookies.set(id, settings.get(id), 'max');
 			});
 	},
 	load: function(){
 		$('#settings')
 			.find('input, select').each(function(){
 				var id = $(this).attr('id');
-				var v = window.cookies.get(id);
+				var v = cookies.get(id);
 				if (v !== null)
-					window.settings.set(id, v);
+					settings.set(id, v);
 			});
 	}
 }
@@ -317,7 +308,7 @@ window.settings = {
  * 	copyrighted.
  * http://www.quirksmode.org/js/cookies.html
  * */
-window.cookies = {
+cookies = {
 	set: function(name, value, days) {
 		if (days == 'max')
 			days = 365;
@@ -352,12 +343,4 @@ window.cookies = {
 	unset: function(name) {
 		this.set(name,"",-1);
 	}
-}
-function bindButtons()
-{
-	$('button').unbind('mouseup').mouseup(function(){
-			if ($('.current.freeze').length < 1)
-				next();
-		});
-
 }
