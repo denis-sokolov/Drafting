f3f01e47ca2a44b80403a9d401a232137ddecc0b
Denis Sokolov <denis@sokolov.cc>
2011-07-16 23:37:49 +0300

    Clean up make script.

--- make.py
+++ make.py
@@ -7,8 +7,18 @@ from PyHtmlify.PyCssMagic.CssMagic import CssMagic
 # Compile using msgfmt -o
 
 def prepareOutput():
-	shutil.rmtree('output')
-	os.mkdir('output')
+	# Ensure output directory exists
+	if not os.path.exists('output'):
+		os.mkdir('output')
+		
+	# Ensure it is empty
+	for item in os.listdir('output'):
+		if os.path.isdir('output/%s' % item):
+			shutil.rmtree('output/%s' % item)
+		else:
+			os.remove('output/%s' % item)
+
+	# Copy static files
 	shutil.copytree('js', 'output/js')
 	shutil.copytree('css', 'output/css')
 	shutil.copytree('img', 'output/img')
@@ -23,33 +33,26 @@ def setLang(lang):
 	gettext.bindtextdomain('t', 'output/')
 	gettext.textdomain('t')
 
-def getHtml():
-	with open('drafting.html') as source:
-		result = source.read()
-	return result
-
-def keyword(key):
-	return gettext.gettext(key.group(1))
-
-
-
-
-
 if __name__ == '__main__':
 	prepareOutput()
 	
+	# Magic with CSS
 	for name in os.listdir('output/css'):
-		print name
 		with open('output/css/%s' % name) as f:
 			css = f.read()
 		with open('output/css/%s' % name, 'w') as f:
 			f.write(CssMagic(css).magic().get())
-		
+	
+	# Do main work
+	with open('drafting.html') as f:
+		source = f.read()
 	for lang in os.listdir('lang'):
 		if lang.endswith('.mo'):
 			lang = lang[:-3]
 			setLang(lang)
-			html = re.sub(r'\[([^]]+)\]', keyword, getHtml())
+			html = re.sub(r'\[([^]]+)\]',
+				lambda key: gettext.gettext(key.group(1)),
+				source)
 			html = html.replace('{lang-code}', lang)
 			html = html.replace('{release-date}', datetime.date.today().strftime('%Y-%m-%d'))
 			html = html.replace('{release-year}', datetime.date.today().strftime('%Y'))
