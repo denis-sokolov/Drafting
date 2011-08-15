d2bbb5763af804ccf0440ef5231d8bb9ff9e1051
Denis Sokolov <denis@sokolov.cc>
2011-08-15 16:33:33 +0300

    Fixed lang-make to be able to run from outside the directory.

--- lang/make.py
+++ lang/make.py
@@ -5,7 +5,7 @@ import commands, os, os.path
 def convert(directory):
 	for item in os.listdir(directory):
 		if item.endswith('.po'):
-			commands.getoutput('msgfmt -o %s.mo %s' % (item[:-3], item))
+			commands.getoutput('msgfmt -o %s.mo %s' % (os.path.join(directory, item[:-3]), os.path.join(directory, item)))
 
 if __name__=='__main__':
 	convert(os.path.abspath(os.path.dirname(__file__)))
